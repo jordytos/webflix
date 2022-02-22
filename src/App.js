081f297d5b7e3735de50f6b1e971e4a27284c785
jordytos <jordynabina@gmail.com>
2022-02-22 01:35:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter,Link, Route } from 'react-router-dom';
+import About from './About';
 import './App.css';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <BrowserRouter>
+      <header>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">À propos</Link>
+        </nav>
       </header>
-    </div>
-  );
+      <main>
+        <Route path="/" element={<Home />} />
+        <Route path="/about">
+          <About />
+        </Route>
+      </main>
+    </BrowserRouter> 
+  )
 }
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,6 @@
+function Home(){
+    return <h1>WebFlix <span role="img" aria-label="popcorn"></span></h1>;
+}
+
+
+export default Home;
